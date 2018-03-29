ea76ad8284b4d9191556e7ff9471d527c3cb9644
JohnnysMartins <johnnys.adson@gmail.com>
2018-03-29 19:44:42 -0300

    removing errors from private attributes.

--- lib/hell_triangle.py
+++ lib/hell_triangle.py
@@ -4,24 +4,24 @@ class HellTriangle:
         self.__triangle = triangle
 
     def total_sum(self):
-        if self.is_triangle_valid():
-            return self.total_calculates()
+        if self.__is_triangle_valid():
+            return self.__total_calculates()
         return None
 
     def __total_calculates(self):
         total = 0
-        for row in self.triangle:
+        for row in self.__triangle:
             for item in row:
                 total += item
         return total
 
     def __is_triangle_valid(self):
-        if not self.triangle or not isinstance(self.triangle, list):
+        if not self.__triangle or not isinstance(self.__triangle, list):
             return False
-        return self.is_row_valid()
+        return self.__is_row_valid()
 
     def __is_row_valid(self):
-        for row in self.triangle:
+        for row in self.__triangle:
             if not row or not isinstance(row, list):
                 return False
             for item in row:
