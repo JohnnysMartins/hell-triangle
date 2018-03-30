9f9522178e0e2fd454bf74786b4937aa0e17c6bb
JohnnysMartins <johnnys.adson@gmail.com>
2018-03-30 01:05:11 -0300

    implementation without repeated numbers in the same list

--- lib/hell_triangle.py
+++ lib/hell_triangle.py
@@ -1,19 +1,25 @@
 class HellTriangle:
 
-    def __init__(self, triangle):
+    def __init__(self, triangle=[]):
         self.__triangle = triangle
+        self.__value_max = 0
+        self.__index_last_value = 0
 
     def total_sum(self):
         if self.__is_triangle_valid():
-            return self.__total_calculates()
+            self.__total_calculates()
+            return self.value_max
         return None
 
     def __total_calculates(self):
-        total = 0
+        list_to_sum = []
         for row in self.__triangle:
-            for item in row:
-                total += item
-        return total
+            new_list = row[self.__index_last_value: self.__index_last_value + 2]
+            value = max(new_list)
+            list_to_sum.append(value)
+            self.__index_last_value = row.index(value)
+
+        self.value_max = sum(list_to_sum)
 
     def __is_triangle_valid(self):
         if not self.__triangle or not isinstance(self.__triangle, list):
--- test/test_hell_triangle.py
+++ test/test_hell_triangle.py
@@ -4,9 +4,13 @@ from lib.hell_triangle import HellTriangle
 
 class HellTriangleTest(unittest.TestCase):
 
-    def test_triangle_isNotNone(self):
+    def test_triangle_is_not_none(self):
         triangle = HellTriangle([])
         self.assertIsNotNone(triangle)
+
+    def test_total_sum_is_none(self):
+        triangle = HellTriangle([])
+        self.assertIsNone(triangle.total_sum())
 
     def test_triangle_with_letter(self):
         triangle = HellTriangle([[1], [2, 'b']])
@@ -14,7 +18,11 @@ class HellTriangleTest(unittest.TestCase):
 
     def test_triangle_total_sum(self):
         triangle = HellTriangle([[1], [3, 7]])
-        self.assertEqual(11, triangle.total_sum())
+        self.assertEqual(8, triangle.total_sum())
+
+    def test_triangle_total_sum(self):
+        triangle = HellTriangle([[1], [3, 7], [9, 7, 1]])
+        self.assertEqual(15, triangle.total_sum())
 
 
 if __name__ == '__main__':
