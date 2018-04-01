1e489de41047798c9d5ae890fe0346c7ecfb0b35
JohnnysMartins <johnnys.adson@gmail.com>
2018-04-01 01:43:16 -0300

    refactoring the number validation inside the row in a method.

--- lib/hell_triangle.py
+++ lib/hell_triangle.py
@@ -1,6 +1,6 @@
 class HellTriangle:
 
-    def __init__(self, triangle=[]):
+    def __init__(self, triangle):
         self.__triangle = triangle
         self.__value_max = 0
 
@@ -14,7 +14,8 @@ class HellTriangle:
         while len(self.__triangle) > 1:
             last_row = self.__triangle.pop()
             last_but_one_row = self.__triangle.pop()
-            value = [max(last_row[index], last_row[index + 1]) + number for index, number in enumerate(last_but_one_row)]
+            value = [max(last_row[index], last_row[index + 1]) + number for index, number in
+                     enumerate(last_but_one_row)]
             self.__triangle.append(value)
         self.__value_max = self.__triangle[0][0]
 
@@ -27,7 +28,11 @@ class HellTriangle:
         for index, row in enumerate(self.__triangle):
             if not isinstance(row, list) or not len(row) == index + 1:
                 return False
-            for item in row:
-                if not isinstance(item, int):
-                    return False
+            if not self.__is_item_valid(row):
+                return False
+        return True
+
+    def __is_item_valid(self, row):
+        if not all(isinstance(item, int) for item in row):
+            return False
         return True
